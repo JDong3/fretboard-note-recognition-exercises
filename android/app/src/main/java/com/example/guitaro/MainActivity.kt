ebfac033d89accbfb6afe3ddb051b89f72e89393
Simon Wang <mtwangyugeng@gmail.com>
2022-10-13 19:41:10 -0400

    testing compost

--- android/app/src/main/java/com/example/guitaro/MainActivity.kt
+++ android/app/src/main/java/com/example/guitaro/MainActivity.kt
@@ -3,14 +3,18 @@ package com.example.guitaro
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.foundation.Image
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.guitaro.ui.theme.GuitaroTheme
+import androidx.compose.ui.res.painterResource
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,7 +35,21 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    GuitaroTheme {
+        Row {
+            Image(
+                painter = painterResource(R.drawable.profile_picture),
+                contentDescription = "Contact profile picture",
+            )
+            Column {
+                Text(
+                    text = "Hello $name!",
+                    color = MaterialTheme.colorScheme.tertiary;
+                )
+                Text(text = "Hello $name!")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
